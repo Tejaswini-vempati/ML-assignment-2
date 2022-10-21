8e5d8f0b6bc1b6f846c8a8d44a18e22afa3f05bf
Tejaswini-vempati <111906169+Tejaswini-vempati@users.noreply.github.com>
2022-10-20 21:30:34 -0500

    Update que 1.py

--- que 1.py
+++ que 1.py
@@ -1,14 +1,17 @@
-#We have to create an array of 15 elements between 1 & 20 using numpy
 import numpy as np
-Arr= np.random.randint(1,20,15)
-print("Generated array:")
+Arr=np.random.randint(1,20,(15))
 print(Arr)
-#We have to reshape the array and print the reshaped array
-Res=np.reshape(Arr, [3, 5])
-print("Reshaped array: \n",Res)
-#find the shape of the array
-print("Shape of the array ",Res.shape)
-# insert the max value with "0" in each row
-for i in Res:
-    i[i == max(i)] = 0
+
+#Reshape the array to 3 by 5
+Res=Arr.reshape(3,5)
 print(Res)
+
+#Print array shape
+print(np.shape(Res))
+
+#Replace the max in each row by 0
+maxNum = np.amax(Res, axis=1)
+print(maxNum)
+
+d = np.where(np.isin(Res,maxNum), 0, Res)
+print(d)
