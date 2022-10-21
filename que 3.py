d8a46e33f1efa7b2a14dc81394aebb1a13560d60
Tejaswini-vempati <111906169+Tejaswini-vempati@users.noreply.github.com>
2022-10-20 21:31:12 -0500

    Update que 3.py

--- que 3.py
+++ que 3.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
-# Data to plot
+# Data to be inserted
 l = 'Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++'
 p = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
 c = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"]
 # explode 1st slice
 explode = (0.1, 0, 0, 0,0,0)
 # Plot
+a = {'line width':1,'edge color':'black'}
 plt.pie(p, explode=explode, labels=l, colors=c,
 autopct='%1.1f%%', shadow=True, startangle=140)
 plt.axis('equal')
